a84d35170e5db861ac95a932b2f48a2c7fdec216
kelleszzz <627814772@qq.com>
2018-05-31 00:39:34 +0800

    add dependency of FileServerSDK & UserServerSDK

--- src/main/java/com/kelles/crawler/bingcrawler/setting/Setting.java
+++ src/main/java/com/kelles/crawler/bingcrawler/setting/Setting.java
@@ -1,10 +1,13 @@
 package com.kelles.crawler.bingcrawler.setting;
 
 public class Setting {
-    public static final java.lang.String CHROME_DRIVER_PATH ="lib/chromedriver.exe";
-    public static final java.lang.String CHROME_USER_DATA ="lib/ChromeUserData";
+    public static final String CHROME_DRIVER_PATH ="lib/chromedriver.exe";
+    public static final String CHROME_USER_DATA ="lib/ChromeUserData";
 
-    public static final java.lang.String ROOT="CrawledData/";
-    public static final java.lang.String DATA_ANALYSIS="DataAnalysis/";
-    public static final java.lang.String LOG_PATH="MyData/console.log";
+    public static final String ROOT="CrawledData/";
+    public static final String DATA_ANALYSIS="DataAnalysis/";
+    public static final String LOG_PATH=ROOT+"Console.log";
+
+    public static final String USER_ID="BingCrawler";
+    public static final String USER_ACCESS_CODE="tom44123";
 }
